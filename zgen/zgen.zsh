ac94455f0e393c0c468985943a134d609cbcfe98
Paul DelRe <pdelre@gmail.com>
2019-03-08 14:06:19 -0500

    add `zsh-users` shell plugins

--- zgen/zgen.zsh
+++ zgen/zgen.zsh
@@ -4,6 +4,17 @@ if ! zgen saved; then
 
   # specify plugins here
 
+  # If zsh-syntax-highlighting is bundled after zsh-history-substring-search,
+  # they break, so get the order right.
+  zgen load zsh-users/zsh-syntax-highlighting
+  zgen load zsh-users/zsh-history-substring-search
+
+  # Load more completion files for zsh from the zsh-lovers github repo.
+  zgen load zsh-users/zsh-completions src
+
+  # Add Fish-like autosuggestions to your ZSH.
+  zgen load zsh-users/zsh-autosuggestions
+
   # generate the init script from plugins above
   zgen save
 fi
