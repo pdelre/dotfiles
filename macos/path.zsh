31d95b198a70986e4f19b961b12e6ad0f3192c8a
Paul DelRe <pdelre@gmail.com>
2019-03-12 11:43:41 -0400

    remoe `brew --prefix` usage to speed up session start

--- macos/path.zsh
+++ macos/path.zsh
@@ -8,10 +8,12 @@ fi
 export LS_BIN=/usr/local/bin/gls
 
 # Add php path
-local php_path=$(brew --prefix 'php@7.2')
+# local php_path=$(brew --prefix 'php@7.2')
+local php_path='/usr/local/opt/php@7.2'
 export PATH="${php_path}/bin:$PATH"
 export PATH="${php_path}/sbin:$PATH"
 
 # Add ruby path
-local ruby_path=$(brew --prefix ruby)
+# local ruby_path=$(brew --prefix ruby)
+local ruby_path='/usr/local/opt/ruby'
 export PATH="${ruby_path}/bin:$PATH"
