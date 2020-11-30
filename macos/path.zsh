d4463921914b9022782d207fc960601db7433266
Paul DelRe <pdelre@gmail.com>
2020-11-30 10:50:13 -0500

    update `PATH` to use `php@7.4` location

--- macos/path.zsh
+++ macos/path.zsh
@@ -9,7 +9,7 @@ export LS_BIN=/usr/local/bin/gls
 
 # Add php path
 # local php_path=$(brew --prefix 'php@7.2')
-local php_path='/usr/local/opt/php@7.2'
+local php_path='/usr/local/opt/php@7.4'
 export PATH="${php_path}/bin:$PATH"
 export PATH="${php_path}/sbin:$PATH"
 
