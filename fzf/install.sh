101540101bbef32f539ed76838ed6781061d88fe
Paul DelRe <pdelre@gmail.com>
2021-10-06 16:23:57 -0400

    ignore `fzf` if `brew` isn't available

--- fzf/install.sh
+++ fzf/install.sh
@@ -3,5 +3,10 @@
 # @see https://github.com/junegunn/fzf#using-homebrew-or-linuxbrew
 #
 
+if test ! $(which brew)
+then
+  exit 0
+fi
+
 # PDR: not sure if this is needed with brew given I'm only using --bin and keeping the config in dotfiles
 $(brew --prefix)/opt/fzf/install --bin
