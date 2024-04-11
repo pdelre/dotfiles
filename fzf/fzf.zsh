4d3435eb2c8015baad90670947d1cdbcaf47c549
Paul DelRe <pdelre@gmail.com>
2024-04-11 17:19:54 +0000

    update `fzf` for Linux Homebrew

--- fzf/fzf.zsh
+++ fzf/fzf.zsh
@@ -1,13 +1,22 @@
+# install path
+if test "$(uname)" = "Darwin"
+then
+  local fzf_path=/usr/local/opt/fzf
+elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
+then
+  local fzf_path=/home/linuxbrew/.linuxbrew/opt/fzf
+fi
+
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == *${fzf_path}/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}${fzf_path}/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${fzf_path}/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "${fzf_path}/shell/key-bindings.zsh"
