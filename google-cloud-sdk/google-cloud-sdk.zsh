3c27b2796a03f7cc7f3653cf80501a842bd74501
Paul DelRe <pdelre@gmail.com>
2024-04-11 17:24:46 +0000

    noop `google-cloud-sdk`

---
+++ google-cloud-sdk/google-cloud-sdk.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+# Not currently used
+return
+
+if test -f "/usr/local/Caskroom/google-cloud-sdk"
+then
+    source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+    source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+fi
