46b3f72c5a7c73150f2a257597508bbae7bf1dd0
Paul DelRe <pdelre@gmail.com>
2025-04-10 09:07:06 -0400

    update homebrew PATH on mac silicon

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -3,3 +3,15 @@ then
     export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
     export PATH="/home/linuxbrew/.linuxbrew/sbin:$PATH"
 fi
+# Mac Silicon location
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/sbin:$PATH"
+export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
+
+if type brew &>/dev/null
+then
+  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+
+  autoload -Uz compinit
+  compinit
+fi
