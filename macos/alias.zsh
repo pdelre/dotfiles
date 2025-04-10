49305be2bb52cf625b171f89989f2f3ca534ced5
Paul DelRe <pdelre@gmail.com>
2025-04-10 09:07:28 -0400

    remove gnu aliases

--- macos/alias.zsh
+++ macos/alias.zsh
@@ -6,18 +6,18 @@ if test ! "$(uname)" = "Darwin"
 fi
 
 # TODO: look into /usr/local/opt/coreutils/libexec/gnubin
-alias du='/usr/local/bin/gdu'
-alias date='/usr/local/bin/gdate'
-alias df='/usr/local/bin/gdf'
-alias echo='/usr/local/bin/gecho'
-alias touch='/usr/local/bin/gtouch'
+#alias du='/usr/local/bin/gdu'
+#alias date='/usr/local/bin/gdate'
+#alias df='/usr/local/bin/gdf'
+#alias echo='/usr/local/bin/gecho'
+#alias touch='/usr/local/bin/gtouch'
 
 # TODO: look into /usr/local/opt/gnu-tar/libexec/gnubin
-alias tar='/usr/local/bin/gtar'
+#alias tar='/usr/local/bin/gtar'
 
 # GNU
-alias xargs='/usr/local/bin/gxargs'
-alias sed='/usr/local/bin/gsed'
+#alias xargs='/usr/local/bin/gxargs'
+#alias sed='/usr/local/bin/gsed'
 
 # Brew
 alias brew-cask-outdated='brew cask list | xargs brew cask info | grep -B3 "Not installed"'
