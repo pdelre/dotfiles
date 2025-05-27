19c60cb6cb89298726dc0c6efd54afff69350c3c
Paul DelRe <pdelre@gmail.com>
2025-05-27 11:28:56 -0400

    update homebrew PATH for `php`, `ruby`, & `curl`

--- macos/path.zsh
+++ macos/path.zsh
@@ -9,18 +9,21 @@ export LS_BIN=/usr/local/bin/gls
 
 # Add php path
 # local php_path=$(brew --prefix 'php@7.4')
-local php_path='/usr/local/opt/php@8.1'
+# local php_path='/usr/local/opt/php@8.1'
 # local php_path='/usr/local/opt/php@7.4'
-export PATH="${php_path}/bin:$PATH"
-export PATH="${php_path}/sbin:$PATH"
+# export PATH="${php_path}/bin:$PATH"
+# export PATH="${php_path}/sbin:$PATH"
 
 # Add ruby path
 # local ruby_path=$(brew --prefix ruby)
-local ruby_path='/usr/local/opt/ruby'
+local ruby_path='/opt/homebrew/opt/ruby'
 export PATH="${ruby_path}/bin:$PATH"
 
 # Add python3 path for `pip3 install --user` packages
-local python3_path='/Users/pdelre/Library/Python/3.7'
-export PATH="${python3_path}/bin:$PATH"
+# local python3_path='/Users/pdelre/Library/Python/3.9'
+# export PATH="${python3_path}/bin:$PATH"
+
+# Add curl path
+export PATH="/opt/homebrew/opt/curl/bin:$PATH"
 
 export PATH="/usr/local/bin:/usr/local/sbin:${HOME}/bin:$PATH"
